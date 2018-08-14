72df2f7d1e19ecd038940f4d408a24ad4b18855a
tozetao <tozetao@163.com>
2018-08-15 00:09:15 +0800

    8-15

--- lock/lock.php
+++ lock/lock.php
@@ -1,16 +1,7 @@
 <?php
 require dirname(__DIR__) . '/common.php';
 
-/**
- * 对一个key进行加锁操作.
- * 成功将会返回加锁的key所对应的值，即uuid；
- * 获取锁失败的话将会在过期时间内尝试获取加锁操作，直到时间过期或者加锁成功
- *
- * @param $lockName
- * @param int $timeout    锁的超时时间，同时也决定了程序获取锁阻塞的最大时间
- * @return bool|string
- */
-function acquireLock($lockName, $timeout = 10)
+function acquireLock($lockName, $timeout = 30)
 {
     $redis = Singer::getInstance();
 
@@ -31,43 +22,29 @@ function acquireLock($lockName, $timeout = 10)
         else if($redis->ttl($key) === -2)
             $redis->expire($key, $timeout);
 
-        //睡眠0.001秒，1秒 = 1000000微秒
-        usleep(1000);
+        //睡眠0.01秒，1秒 = 1000000微秒
+        usleep(10000);
     }
 
     return false;
 }
 
-/**
- * 释放一个锁
- * @param $lockName
- * @param $uuid
- */
 function releaseLock($lockName, $uuid)
 {
-    //监视加锁的key，取出加锁key的值与传递的值进行对比，如果相同则删除加锁的key并取消监视的key
-
     $redis = Singer::getInstance();
     $lockName = 'lock:' . $lockName;
 
-    while(true)
+    $redis->watch($lockName);
+    $values = $redis->multi()->get($lockName)->exec();
+
+    if($values && $values[0] === $uuid)
     {
-        $redis->watch($lockName);
-        if($redis->get($lockName) == $uuid)
-        {
-            $redis->multi();
-            $redis->delete($lockName);
-            $redis->exec();
-            return true;
-        }
-        $redis->unwatch();
-        break;
+        $redis->delete($lockName);
+        return true;
     }
 
+    $redis->unwatch();
     return false;
-
-//这里写个while循环不是很懂
-//监视加锁的key是为了防止key的值被修改，导致误删除key。
 }
 
 function testLock()
@@ -84,14 +61,10 @@ function testLock()
     //执行一系列操作
     $redis = Singer::getInstance();
     $redis->multi()->set('address', 'shanghai')->exec();
+
     sleep(5);
     $result = releaseLock($rankKey, $uuid);
     var_dump($result);
 }
 
-$redis = Singer::getInstance();
-$r = $redis->ttl('type');
-var_dump($r);
-
-//var_dump($redis->get('lock:rank'));
-//testLock();
+testLock();
--- lock/semaphore.php
+++ lock/semaphore.php
@@ -1,20 +1,61 @@
 <?php
 include_once "../common.php";
 
-function lock()
+//function acquireSemaphore($key, $limit, $timeout=10){}
+
+function semaphoreLock($key, $limit, $timeout = 10)
 {
+    $key = 'lock:' . $key;
+    $uuid = uniqid();
+    $now = time();
+
     $redis = Singer::getInstance();
-    var_dump($redis);
+    $redis->multi();
 
-    $redis->multi()
-        ->set('name', 'zhangsan')
-        ->get('name');
-
-    $redis->zAdd('rank', 2001.1, 'zhangsan');
-
+    //移除过期的成员
+    $redis->zremrangebyscore($key, '-inf', $now - $timeout);
+    $redis->zadd($key, $now, $uuid,);
+    $redis->zrank($key, $uuid);
     $result = $redis->exec();
+    if($result && $result[2] < $limit)
+        return $uuid;
 
-    var_dump($result);
+    $redis->zrem($key, $uuid);
+    return false;
 }
 
-lock();
+function releaseLock($key, $uuid)
+{
+    $key = 'lock:' . $key;
+    $redis = Singer::getInstance();
+    return $redis->zRem($key, $uuid);
+}
+
+function test()
+{
+    $uuid = semaphoreLock('test', 1);
+
+    var_dump($uuid);
+    sleep(5);
+
+    $r = releaseLock('test', $uuid);
+    var_dump($r);
+}
+
+
+test();
+
+/*
+$obj = Singer::getInstance();
+$obj->zAdd('r1', time(), 'z1');
+$obj->zAdd('r1', time(), 'z2');
+
+$r = $obj->zRem('r1', 'z1');
+var_dump($r);
+
+$r = $obj->zRem('r1', 'z2');
+var_dump($r);
+
+$r = $obj->zRem('r1', 'z3');
+var_dump($r);
+*/
