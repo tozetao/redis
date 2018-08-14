c7a30b78a45a6a8511ac5290fe3dace956d4b484
tozetao <tozetao@163.com>
2018-08-14 19:21:12 +0800

    8-14

--- lock/semaphore.php
+++ lock/semaphore.php
@@ -3,6 +3,18 @@ include_once "../common.php";
 
 function lock()
 {
-    $redis = getRedis();
-    $redis->zInter()
+    $redis = Singer::getInstance();
+    var_dump($redis);
+
+    $redis->multi()
+        ->set('name', 'zhangsan')
+        ->get('name');
+
+    $redis->zAdd('rank', 2001.1, 'zhangsan');
+
+    $result = $redis->exec();
+
+    var_dump($result);
 }
+
+lock();
